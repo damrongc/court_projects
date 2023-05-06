44ec16a7dd9c23f7a1e59f401283425c4d5ebe82
Thamolwan Chumni <katiptc@Thamolwans-MacBook-Pro.local>
2023-05-07 00:20:03 +0700

    Update CaseResultsController

---
+++ src/CourtJustice/CourtJustice.Web/Controllers/CaseResultsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CourtJustice.Domain.Models;
+using CourtJustice.Infrastructure.Helpers;
+using CourtJustice.Infrastructure.Interfaces;
+using CourtJustice.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CourtJustice.Web.Controllers
+{
+
+    public class CaseResultsController : BaseController<CaseResultsController>
+    {
+        private readonly ICaseResultRepository _caseResultRepository;
+        public CaseResultsController(ICaseResultRepository caseResultRepository)
+        {
+            _caseResultRepository = caseResultRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await GetAll());
+        }
+
+        private async Task<List<CaseResult>> GetAll()
+        {
+            var results = await _caseResultRepository.GetAll();
+            return results.ToList();
+        }
+
+        public IActionResult Create()
+        {
+            return View(new CaseResult());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CaseResult model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _caseResultRepository.Create(model);
+                _notify.Success($"{model.CaseResultName} is Created.");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _caseResultRepository.GetByKey(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CaseResult model)
+        {
+            var oldEntity = await _caseResultRepository.GetByKey(id);
+
+            if (oldEntity == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _caseResultRepository.Update(id, model);
+                _notify.Success($"{model.CaseResultName} is Updated");
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+
+
+                await _caseResultRepository.Delete(id);
+                //_notify.Success($"Delete is Success.");
+                var results = await GetAll();
+                var html = RenderRazorViewHelper.RenderRazorViewToString(this, "_ViewTable", results);
+                return new JsonResult(new { isValid = true, message = "", html });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { isValid = false, message = ex.Message });
+
+            }
+
+        }
+    }
+}
+
